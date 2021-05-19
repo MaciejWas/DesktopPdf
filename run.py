9d351ec6b27dae48fcad1fcd8f5172c3b6f4edcd
Maciej Wasilewski <wasylk5431@gmail.com>
2021-05-19 12:54:16 +0200

    Remove redundant code

--- run.py
+++ run.py
@@ -7,7 +7,6 @@ from DesktopPdf.configuration import get_server_port
 
 
 def run_chrome():
-    print("Chrome thread")
     port = get_server_port()
     link = "http://" + app.pass_manager.local_ip + ":" + str(port)
     os.system("google-chrome --app=" + link)
@@ -24,9 +23,7 @@ def main():
         chrome_process = multiprocessing.Process(target=run_chrome)
         chrome_process.start()
 
-    port = get_server_port()
-    app.run(host="0.0.0.0", port=port)
-
+    run_app()
 
 if __name__ == "__main__":
     main()
